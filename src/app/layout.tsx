4ce54b4e9a6a08f8126a1352da487a9d9e0c5073
Jan Zimmermann <jan4995@gmail.com>
2026-05-06 17:10:16 +0200

    dynamic year

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
             {children}
         </main>
         <footer className={"flex items-center justify-center p-8 pt-8 text-sm text-faded-navy"}>
-            Copyright 2024 | Erstellt mit <div className="inline-block cursor-grabbing  mx-1 hover:animate-spin">❤️</div> von Prüfdienst für Blitzschutz
+            Copyright {new Date().getFullYear()} | Erstellt mit <div className="inline-block cursor-grabbing  mx-1 hover:animate-spin">❤️</div> von Prüfdienst für Blitzschutz
         </footer>
         </body>
         </html>
